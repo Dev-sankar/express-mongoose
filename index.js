1dcb32ba483addc4f16b1e32ef1e491a648f7ea8
sivasanger <sivasankarshiva008@gmail.com>
2023-12-07 14:53:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+// DATABASE CONNECT
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+// END CONNECTION
+
+
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3700, () => {
+    console.log(`Server Started at ${3700}`)
+})
+
+
+const routes = require('./routes/routes');
+
+app.use('/api', routes)
+
+
+
